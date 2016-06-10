1bece188f8fba6aa321b91e9090ba909e655251d
Ranieri Althoff <ranisalt@gmail.com>
2016-06-10 19:41:27 -0300

    Fix scrapy issues

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-__author__ = 'ranisalt'
-
-def read(file):
-    with open(file) as f:
-        content = f.read()
-    return content
 
 setup(
     name='moita-scrapy',
-    version='0.1.0',
-    description='Simple scraper for UFSC list of subjects and classes with support to MongoDB',
+    version='0.1.1',
+    author='Ranieri Althoff',
+    author_email='ranisalt@gmail.com',
+    description='Scraper for UFSC table of subjects, classes and timetables',
     license='MIT',
-    author=__author__,
     url='https://github.com/ranisalt/moita-scrapy',
-    install_requires=read('requirements.txt'),
+    packages=find_packages(),
 )
